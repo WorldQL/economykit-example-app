4a978b6fb07c22fd11eb15a99d0578d7285fefe4
Jack Baron <jackmbaron@gmail.com>
2022-08-23 17:57:29 +0100

    login backend route

---
+++ lib/economykit.ts
@@ -0,0 +1,92 @@
+import Axios, { type AxiosError } from 'axios'
+import { env } from 'node:process'
+import { URL } from 'node:url'
+
+const { ECONOMYKIT_APP_URL, ECONOMYKIT_APP_TOKEN } = env
+export const baseURL = new URL(
+  ECONOMYKIT_APP_URL ?? 'https://app.economykit.com/'
+).toString()
+
+if (!ECONOMYKIT_APP_TOKEN) {
+  throw new Error('ECONOMYKIT_APP_TOKEN not specified')
+}
+
+const axios = Axios.create({
+  baseURL,
+  headers: {
+    Authorization: `Bearer ${ECONOMYKIT_APP_TOKEN}`,
+  },
+})
+
+// @ts-expect-error Type Assertion
+export const isAxiosError: (error: unknown) => error is AxiosError = error => {
+  if (typeof error !== 'object') return false
+  if (error === null) return false
+
+  // @ts-expect-error Property might not exist
+  return error.isAxiosError === true
+}
+
+enum ExternalIdentifier {
+  STEAM = 'ST',
+  EPIC_GAMES = 'EG',
+  IOS_GAME_CENTER = 'GC',
+  GOOGLE_PLAY_GAMES_SERVICES = 'GS',
+  MINECRAFT = 'MC',
+  EMAIL = 'EM',
+  DISCORD = 'DS',
+  CUSTOM = 'XX',
+}
+
+interface PlayerIDRequest {
+  display_name?: string
+  id_type: ExternalIdentifier
+  id_payload: string
+}
+
+interface PlayerIDResponse {
+  player_id: string
+  display_name: string
+}
+
+export const playerID = async (name: string) => {
+  const body: PlayerIDRequest = {
+    display_name: name,
+    id_type: ExternalIdentifier.CUSTOM,
+    id_payload: name,
+  }
+
+  const { data } = await axios.post<PlayerIDResponse>(
+    '/inventories/api/v1/player-id/',
+    body
+  )
+
+  return data
+}
+
+interface ProvisionTokenRequest {
+  player?: string
+  expiry: string
+}
+
+interface ProvisionTokenResponse {
+  key: string
+}
+
+export const provisionToken = async (expireTimeMS: number, player?: string) => {
+  const now = Date.now()
+  const future = now + expireTimeMS
+  const date = new Date(future)
+
+  const body: ProvisionTokenRequest = {
+    expiry: date.toISOString(),
+    player,
+  }
+
+  const { data } = await axios.post<ProvisionTokenResponse>(
+    '/inventories/api/v1/api-key/',
+    body
+  )
+
+  return data
+}
--- pages/api/login.ts
+++ pages/api/login.ts
@@ -1,22 +1,39 @@
 import { ReasonPhrases, StatusCodes } from 'http-status-codes'
+import ms from 'ms'
 import { type NextApiRequest, type NextApiResponse } from 'next'
 import nc from 'next-connect'
-import { env } from 'node:process'
+import { baseURL, playerID, provisionToken } from '~/lib/economykit'
+
+const stringParameter = (request: NextApiRequest, key: string) => {
+  const value = request.body[key] as unknown
+  if (typeof value !== 'string') return undefined
+
+  return value
+}
+
+// Hardcoded expiry time
+const expires = ms('72')
 
 const handler = nc<NextApiRequest, NextApiResponse>()
-
 handler.post(async (request, resp) => {
-  const { ECONOMYKIT_APP_TOKEN } = env
-  if (!ECONOMYKIT_APP_TOKEN) {
-    resp.status(StatusCodes.INTERNAL_SERVER_ERROR)
-    resp.send(ReasonPhrases.INTERNAL_SERVER_ERROR)
+  const name = stringParameter(request, 'name')
+  if (!name) {
+    resp.status(StatusCodes.BAD_REQUEST)
+    resp.send(ReasonPhrases.BAD_REQUEST)
 
     return
   }
 
-  // TODO
-  resp.status(StatusCodes.NOT_IMPLEMENTED)
-  resp.send(ReasonPhrases.NOT_IMPLEMENTED)
+  const idResp = await playerID(name)
+  const tokenResp = await provisionToken(expires, idResp.player_id)
+
+  resp.send({
+    id: idResp.player_id,
+    displayName: idResp.display_name,
+    token: tokenResp.key,
+
+    baseURL,
+  })
 })
 
 export default handler
