8c7d3a6bcfd67c3b22c6b42a09d97f19fe7dc162
Jack Baron <jackmbaron@gmail.com>
2022-08-24 14:36:28 +0100

    start fleshing out trade interface

--- components/economykit/Trade.tsx
+++ components/economykit/Trade.tsx
@@ -1,11 +1,89 @@
 import { type FC } from 'react'
+import { Card } from '~/components/ui/Card'
+import { Inventory } from '~/lib/economykit/inventory'
+import { BlankItem } from './BlankItem'
+import { CommodityStack } from './CommodityStack'
+import {
+  ExpandingItemGrid,
+  type ExpandingItemGridProps,
+} from './ExpandingItemGrid'
+import { ItemGrid, type ItemGridProps } from './ItemGrid'
+import { UniqueItem } from './UniqueItem'
 
 interface Props {
-  // TODO: Match to API model
-  originator: unknown
-  recipient: unknown
+  originator: Inventory
+  recipient: Inventory
 }
 
-export const Inventory: FC<Props> = ({ originator, recipient }) => (
-  <div>Inventory</div>
+export const Trade: FC<Props> = ({ originator, recipient }) => {
+  // TODO: Drag and drop
+
+  return (
+    <Card>
+      <div className='flex'>
+        <div className='flex flex-col gap-6'>
+          <h2 className='-mb-3 text-center text-lg font-semibold'>
+            Your Items
+          </h2>
+          <DragItemGrid
+            height={3}
+            uniqueItems={originator.uniqueItems}
+            commodityStacks={originator.commodityStacks}
+          />
+
+          <DragExpandingItemGrid
+            uniqueItems={originator.uniqueItems}
+            commodityStacks={originator.commodityStacks}
+          />
+        </div>
+
+        <div className='flex-grow' />
+
+        <div className='flex flex-col gap-6'>
+          <h2 className='-mb-3 text-center text-lg font-semibold'>
+            Their Items
+          </h2>
+
+          <DragItemGrid
+            height={3}
+            uniqueItems={recipient.uniqueItems}
+            commodityStacks={recipient.commodityStacks}
+          />
+
+          <DragExpandingItemGrid
+            uniqueItems={recipient.uniqueItems}
+            commodityStacks={recipient.commodityStacks}
+          />
+        </div>
+      </div>
+    </Card>
+  )
+}
+
+type DragGridProps = Omit<
+  ItemGridProps,
+  'commodityStack' | 'uniqueItem' | 'blankItem'
+>
+
+const DragItemGrid: FC<DragGridProps> = ({ ...props }) => (
+  <ItemGrid
+    uniqueItem={item => <UniqueItem key={item.id} {...item} />}
+    commodityStack={item => <CommodityStack key={item.id} {...item} />}
+    blankItem={idx => <BlankItem key={idx} />}
+    {...props}
+  />
+)
+
+type DragExpandingGridProps = Omit<
+  ExpandingItemGridProps,
+  'commodityStack' | 'uniqueItem' | 'blankItem'
+>
+
+const DragExpandingItemGrid: FC<DragExpandingGridProps> = ({ ...props }) => (
+  <ExpandingItemGrid
+    uniqueItem={item => <UniqueItem key={item.id} {...item} />}
+    commodityStack={item => <CommodityStack key={item.id} {...item} />}
+    blankItem={idx => <BlankItem key={idx} />}
+    {...props}
+  />
 )
---
+++ pages/trade/create/[recipient].tsx
@@ -0,0 +1,39 @@
+import { type NextPage } from 'next'
+import Head from 'next/head'
+import useSWR from 'swr'
+import { Trade } from '~/components/economykit/Trade'
+import { Loading } from '~/components/views/Loading'
+import { Page } from '~/components/views/Page'
+import { inventory as fetchInventory } from '~/lib/economykit/inventory'
+import { useEnsureAuth } from '~/lib/hooks/useAuth'
+
+const TradeCreate: NextPage = () => {
+  const auth = useEnsureAuth()
+
+  const { data: originator } = useSWR(
+    auth && ['/inventory', auth],
+    async (_, auth) => fetchInventory(auth)
+  )
+
+  if (!auth || !originator) return <Loading />
+
+  const { displayName } = auth
+
+  return (
+    <>
+      <Head>
+        <title>EconomyKit Example App | Create Trade</title>
+      </Head>
+
+      <Page username={displayName}>
+        <Trade
+          originator={originator}
+          // TODO: Retrieve recipient's items
+          recipient={{ uniqueItems: [], commodityStacks: [] }}
+        />
+      </Page>
+    </>
+  )
+}
+
+export default TradeCreate
