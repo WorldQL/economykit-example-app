9b59f2f09d73f34c533762617928f676b3b4ce57
Jack Baron <jackmbaron@gmail.com>
2022-08-24 14:30:37 +0100

    use new item grid components

--- components/economykit/Inventory.tsx
+++ components/economykit/Inventory.tsx
@@ -10,29 +10,24 @@ import { ItemGrid } from './ItemGrid'
 export const Inventory: FC<InventoryModel> = ({
   uniqueItems,
   commodityStacks,
-}) => {
-  const { items, page, pages, firstPage, lastPage, nextPage, previousPage } =
-    useItemGrid(uniqueItems, commodityStacks)
+}) => (
+  <Card>
+    <div className='flex flex-col gap-4'>
+      <h2 className='text-xl font-bold'>Inventory</h2>
 
-  return (
-    <Card>
-      <div className='flex flex-col gap-4'>
-        <h2 className='text-xl font-bold'>Inventory</h2>
+      <div className='flex w-full gap-6'>
+        <ItemGrid
+          uniqueItems={uniqueItems}
+          commodityStacks={commodityStacks}
+          uniqueItem={item => <UniqueItem key={item.id} {...item} />}
+          commodityStack={item => <CommodityStack key={item.id} {...item} />}
+          blankItem={idx => <BlankItem key={idx} />}
+        />
 
-        <div className='flex w-full gap-6'>
-          <ItemGrid
-            uniqueItems={uniqueItems}
-            commodityStacks={commodityStacks}
-            uniqueItem={item => <UniqueItem key={item.id} {...item} />}
-            commodityStack={item => <CommodityStack key={item.id} {...item} />}
-            blankItem={idx => <BlankItem key={idx} />}
-          />
-
-          <div className='flex-grow rounded m-1 p-2 outline outline-offset-2 outline-gray-400'>
-            content
-          </div>
+        <div className='flex-grow rounded m-1 p-2 outline outline-offset-2 outline-gray-400'>
+          content
         </div>
       </div>
-    </Card>
-  )
-}
+    </div>
+  </Card>
+)
